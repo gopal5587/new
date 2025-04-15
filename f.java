8ec37de07ef7f0550beb74c04c47969293e56cbb
gopalyadav <gopal5587yadav@gmail.com>
2025-04-16 01:32:35 +0530

    g

--- f.java
+++ f.java
@@ -2,7 +2,7 @@ package project;
 
 public class f {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world")
     }
     
 }
